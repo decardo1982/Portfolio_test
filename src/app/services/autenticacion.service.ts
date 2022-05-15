9ff7b2a884dfbcac4510a8eac7b9b64b44fd6385
Dario <dario.cardozo@hotmail.com>
2022-05-15 17:20:27 -0300

    carpeta frontend

---
+++ src/app/services/autenticacion.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacionService {
+  url="http://npinti.ddns.net:9008/api/auth/login";
+  constructor(private http:HttpClient) { }
+}
